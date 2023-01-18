cacf294531e81fcf98493a79d36e276357d15944
marcin10lw <marcin10lw@wp.pl>
2023-01-18 19:59:06 +0100

    created Hero

---
+++ src/Hero/index.js
@@ -0,0 +1,19 @@
+import { StyledHero } from "./styled";
+import { Button } from "../Button/styled";
+
+const Hero = () => {
+  return (
+    <StyledHero>
+      <h1>Build The Community Your Fans Will Love</h1>
+      <p>
+        Huddle re-imagines the way we build communities. You have a voice, but
+        so does your audience. Create connections with your users as you engage
+        in genuine discussion.
+      </p>
+      <Button inMain>Get Started For Free</Button>
+      <img src={`${process.env.PUBLIC_URL}/assets/images/screen-mockups.svg`} />
+    </StyledHero>
+  );
+};
+
+export default Hero;
