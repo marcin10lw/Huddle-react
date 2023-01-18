95812c98fae5ddd10c77ecdc5bdc039252afad0a
marcin10lw <marcin10lw@wp.pl>
2023-01-18 18:28:57 +0100

    created Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Container } from "./Container/styled";
 import Header from "./Header";
+import Main from "./Main";
 
 function App() {
   return (
     <>
       <Container>
         <Header />
+        <Main />
       </Container>
     </>
   );
---
+++ src/Main/index.js
@@ -0,0 +1,9 @@
+import { StyledMain } from "./styled";
+
+const Main = () => {
+  return (
+    <StyledMain></StyledMain>
+  );
+};
+
+export default Main;
