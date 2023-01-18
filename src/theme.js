2b6a9fc5deed7e000f3a9d25ed71ffee660136e7
marcin10lw <marcin10lw@wp.pl>
2023-01-18 08:24:14 +0100

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    pink: "hsl(322, 100%, 66%)",
+    lightPink: "hsl(321, 100%, 78%)",
+    lightRed: "hsl(0, 100%, 63%)",
+    vdCyan: "hsl(192, 100%, 9%)",
+    vpBlue: "hsl(207, 100%, 98%)",
+  },
+  mobile: 767,
+};
