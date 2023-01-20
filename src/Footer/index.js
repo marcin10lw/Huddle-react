3d9f297a1215c3bc68777acae1e973752d5cccb9
marcin10lw <marcin10lw@wp.pl>
2023-01-20 15:01:35 +0100

    created Footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,13 +1,25 @@
-import { StyledFooter, StylingImage } from "./styled";
+import { StyledFooter, StylingPicture, Wrapper } from "./styled";
+import Contact from "./Contact";
+import Newsletter from "./Newsletter";
 
 const Footer = () => {
   return (
     <>
-      <StylingImage
-        src={`${process.env.PUBLIC_URL}/assets/images/bg-footer-top-desktop.svg`}
-      />
+      <StylingPicture>
+        <source
+          srcSet={`${process.env.PUBLIC_URL}/assets/images/bg-footer-top-mobile.svg`}
+          media="(max-width: 767px)"
+        />
+        <img
+          src={`${process.env.PUBLIC_URL}/assets/images/bg-footer-top-desktop.svg`}
+          alt=""
+        />
+      </StylingPicture>
       <StyledFooter>
-        
+        <Wrapper>
+          <Contact />
+          <Newsletter />
+        </Wrapper>
       </StyledFooter>
     </>
   );
--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -2,11 +2,34 @@ import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
   background-color: ${({ theme }) => theme.colors.vdCyan};
-  height: 600px;
+  color: ${({ theme }) => theme.colors.vpBlue};
+  padding: 120px 38px;
+  line-height: 1.6;
+
+  p {
+    font-size: 0.875rem;
+    font-size: clamp(0.875rem, 0.83rem + 0.19vw, 1rem);
+    max-width: 375px;
+    width: 100%;
+  }
 `;
 
-export const StylingImage = styled.img`
+export const StylingPicture = styled.picture`
   display: block;
   margin-top: 70px;
-  width: 100%;
+
+  img {
+    width: 100%;
+    display: block;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  @media (max-width: ${({ theme }) => theme.mobile}px) {
+    flex-direction: column-reverse;
+  }
 `;
