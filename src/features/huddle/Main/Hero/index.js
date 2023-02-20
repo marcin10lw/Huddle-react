ea1e6e6eee152e851d37b2c063be9d3f0be35250
marcin10lw <marcin10lw@wp.pl>
2023-02-20 19:42:28 +0100

    used all styled components in Hero

--- src/features/huddle/Main/Hero/index.js
+++ src/features/huddle/Main/Hero/index.js
@@ -1,18 +1,18 @@
-import { StyledHero } from "./styled";
-import { Button } from "../../Button/styled";
+import { StyledHero, HeroHeader, HeroText, HeroImage } from "./styled";
+import { Button } from "../../Button";
 import Statistics from "./Statistics.js";
 
 const Hero = () => {
   return (
     <StyledHero>
-      <h1>Build The Community Your Fans Will Love</h1>
-      <p>
+      <HeroHeader>Build The Community Your Fans Will Love</HeroHeader>
+      <HeroText>
         Huddle re-imagines the way we build communities. You have a voice, but
         so does your audience. Create connections with your users as you engage
         in genuine discussion.
-      </p>
+      </HeroText>
       <Button inMain>Get Started For Free</Button>
-      <img
+      <HeroImage
         src={`${process.env.PUBLIC_URL}/assets/images/screen-mockups.svg`}
         alt="mockup"
       />
--- src/features/huddle/Main/Hero/styled.js
+++ src/features/huddle/Main/Hero/styled.js
@@ -2,23 +2,23 @@ import styled from "styled-components";
 
 export const StyledHero = styled.section`
   text-align: center;
+`;
 
-  h1 {
-    margin: 0;
-    font-size: 1.5625rem;
-    font-size: clamp(1.5625rem, 1.0123rem + 2.3474vw, 3.125rem);
-  }
+export const HeroHeader = styled.h1`
+  margin: 0;
+  font-size: 1.5625rem;
+  font-size: clamp(1.5625rem, 1.0123rem + 2.3474vw, 3.125rem);
+`;
 
-  p {
-    max-width: 660px;
-    margin: 30px auto 0;
-    font-size: 0.875rem;
-    font-size: clamp(0.875rem, 0.72rem + 0.66vw, 1.3125rem);
-  }
+export const HeroText = styled.p`
+  max-width: 660px;
+  margin: 30px auto 0;
+  font-size: 0.875rem;
+  font-size: clamp(0.875rem, 0.72rem + 0.66vw, 1.3125rem);
+`;
 
-  img {
-    max-width: 100%;
-    display: block;
-    margin: 90px auto 0;
-  }
+export const HeroImage = styled.img`
+  max-width: 100%;
+  display: block;
+  margin: 90px auto 0;
 `;
