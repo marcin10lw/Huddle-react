ce54eb4a24ab703eb78698b6ac6b007c02e9b941
marcin10lw <marcin10lw@wp.pl>
2023-02-20 19:41:38 +0100

    removed redundant Button file

--- src/features/huddle/Button/index.js
+++ src/features/huddle/Button/index.js
@@ -1,7 +1,59 @@
-// import { StyledButton } from "./styled";
+import styled, { css } from "styled-components";
 
-// const Button = ({ text }) => {
-//   return <StyledButton>{text}</StyledButton>;
-// };
+const pink = ({ theme }) => theme.colors.pink;
+const lightPink = ({ theme }) => theme.colors.lightPink;
+const vpBlue = ({ theme }) => theme.colors.vpBlue;
+const mobile = ({ theme }) => `${theme.mobile}px`;
 
-// export default Button;
+export const Button = styled.button`
+  border-radius: 20px;
+  cursor: pointer;
+
+  ${({ inHeader }) =>
+    inHeader &&
+    css`
+      border: 1.5px solid ${pink};
+      color: ${pink};
+      padding: 8px 24px;
+      transition: all 120ms ease-out;
+      background-color: transparent;
+
+      &:hover {
+        border: 1.5px solid ${lightPink};
+        color: ${lightPink};
+      }
+
+      @media (max-width: ${mobile}) {
+        padding: 6px 20px;
+        font-size: 12px;
+      }
+    `}
+
+  ${({ inMain }) =>
+    inMain &&
+    css`
+      margin-top: 42px;
+      border: none;
+      background-color: ${pink};
+      color: ${vpBlue};
+      font-size: 20px;
+      font-weight: 600;
+      padding: 26px 100px;
+      border-radius: 40px;
+      box-shadow: 2px 2px 12px 1px hsl(0deg 0% 72%);
+      transition: background-color 120ms ease-out;
+
+      &:hover {
+        background-color: ${lightPink};
+      }
+
+      @media (max-width: ${mobile}) {
+        padding: 14px 40px;
+        font-size: 14px;
+
+        &:last-child {
+          margin-top: 32px;
+        }
+      }
+    `}
+`;
