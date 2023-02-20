9909b8d14b6d3be556c8d77ab203c18794a95745
marcin10lw <marcin10lw@wp.pl>
2023-02-20 20:04:21 +0100

    created separate TextContent component

---
+++ src/features/huddle/Footer/TextContent/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const TextContent = styled.p`
+  font-size: 0.875rem;
+  font-size: clamp(0.875rem, 0.83rem + 0.19vw, 1rem);
+  max-width: 375px;
+  width: 100%;
+  padding-right: 10px;
+`;
