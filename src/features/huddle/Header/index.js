1220b864f3a0baec779099bcb41a3872fe8f9d1a
marcin10lw <marcin10lw@wp.pl>
2023-04-18 23:54:06 +0200

    animated Header and Hero section

--- src/features/huddle/Header/index.js
+++ src/features/huddle/Header/index.js
@@ -1,9 +1,31 @@
 import { StyledHeader, Logo } from "./styled";
 import { Button } from "../Button";
+import { motion } from "framer-motion";
+
+const headerVariants = {
+  hidden: {
+    opacity: 0,
+    y: -20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0.1,
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
   return (
-    <StyledHeader>
+    <StyledHeader
+      as={motion.header}
+      variants={headerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <Logo
         src={`${process.env.PUBLIC_URL}/assets/images/logo.svg`}
         alt="logo"
--- src/features/huddle/Main/Hero/index.js
+++ src/features/huddle/Main/Hero/index.js
@@ -1,10 +1,32 @@
 import { StyledHero, HeroHeader, HeroText, HeroImage } from "./styled";
 import { Button } from "../../Button";
 import Statistics from "./Statistics.js";
+import { motion } from "framer-motion";
+
+const heroVariants = {
+  hidden: {
+    opacity: 0,
+    y: 40,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      delay: 0.4,
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Hero = () => {
   return (
-    <StyledHero>
+    <StyledHero
+      as={motion.section}
+      variants={heroVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <HeroHeader>Build The Community Your Fans Will Love</HeroHeader>
       <HeroText>
         Huddle re-imagines the way we build communities. You have a voice, but
