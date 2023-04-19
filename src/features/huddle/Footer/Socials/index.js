f0e39a276d9c1afcc114d3cea5ece2c786d9eda4
marcin10lw <marcin10lw@wp.pl>
2023-04-20 00:59:05 +0200

    animated Footer with framer motion

--- src/features/huddle/Footer/Socials/index.js
+++ src/features/huddle/Footer/Socials/index.js
@@ -5,19 +5,51 @@ import {
   faSquareInstagram,
   faTwitterSquare,
 } from "@fortawesome/free-brands-svg-icons";
+import { motion } from "framer-motion";
+import useInViewAnimation from "../../useInViewAnimation";
+
+const socialsVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const linkVariants = {
+  hidden: { opacity: 0, x: "-20vw", transition: { duration: 0.4 } },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+};
 
 const Socials = () => {
+  const { animation, ref } = useInViewAnimation(0.4);
+
   return (
-    <StyledSocials>
-      <a href="#">
+    <StyledSocials
+      ref={ref}
+      as={motion.section}
+      variants={socialsVariants}
+      initial="hidden"
+      animate={animation}
+    >
+      <motion.a variants={linkVariants} href="#">
         <FontAwesomeIcon icon={faFacebookSquare} />
-      </a>
-      <a href="#">
+      </motion.a>
+      <motion.a variants={linkVariants} href="#">
         <FontAwesomeIcon icon={faSquareInstagram} />
-      </a>
-      <a href="#">
+      </motion.a>
+      <motion.a variants={linkVariants} href="#">
         <FontAwesomeIcon icon={faTwitterSquare} />
-      </a>
+      </motion.a>
     </StyledSocials>
   );
 };
--- src/features/huddle/Footer/index.js
+++ src/features/huddle/Footer/index.js
@@ -2,8 +2,30 @@ import { StyledFooter, StylingPicture, Wrapper, PictureImage } from "./styled";
 import Contact from "./Contact";
 import Newsletter from "./Newsletter";
 import Socials from "./Socials";
+import useInViewAnimation from "../useInViewAnimation";
+import { motion } from "framer-motion";
+
+const footerVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      stiffness: 50,
+      bounce: 1,
+      mass: 1,
+      delay: 0.1,
+    },
+  },
+};
 
 const Footer = () => {
+  const { animation, ref } = useInViewAnimation(0.4);
+
   return (
     <>
       <StylingPicture>
@@ -17,7 +39,13 @@ const Footer = () => {
         />
       </StylingPicture>
       <StyledFooter>
-        <Wrapper>
+        <Wrapper
+          as={motion.div}
+          ref={ref}
+          variants={footerVariants}
+          initial="hidden"
+          animate={animation}
+        >
           <Contact />
           <Newsletter />
           <Socials />
