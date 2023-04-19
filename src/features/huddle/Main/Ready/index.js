3fc2503356e446bb72d45e1a6c98909ac33ca6da
marcin10lw <marcin10lw@wp.pl>
2023-04-19 23:58:32 +0200

    animated Ready button

--- src/features/huddle/Main/Ready/index.js
+++ src/features/huddle/Main/Ready/index.js
@@ -1,11 +1,55 @@
 import { Wrapper, ReadyHeader } from "./styled";
 import { Button } from "../../Button";
+import { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+
+const readyVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.8,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      stiffness: 50,
+      bounce: 1,
+      mass: 1,
+      delay: 0.1,
+    },
+  },
+};
 
 const Ready = () => {
+  const statisticRef = useRef(null);
+  const inView = useInView(statisticRef, { amount: 0.7, once: false });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start("visible");
+    }
+  }, [animation, inView]);
+
   return (
-    <Wrapper>
+    <Wrapper
+      as={motion.section}
+      ref={statisticRef}
+      variants={readyVariants}
+      initial="hidden"
+      animate={animation}
+    >
       <ReadyHeader>Ready To Build Your Community?</ReadyHeader>
-      <Button inMain>Get Started For Free</Button>
+      <Button
+        as={motion.button}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 200, damping: 10 }}
+        inMain
+      >
+        Get Started For Free
+      </Button>
     </Wrapper>
   );
 };
